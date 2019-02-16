994ae62fabb59cb6a41c4c4cd35551db75b79618
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-16 14:01:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-var addElementToBeginningOfArray = chocolateBars.unshift('skor')
+var addElementToBeginningOfArray = chocolateBars.unshift('skor');
+
 
 
 
